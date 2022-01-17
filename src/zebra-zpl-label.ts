f2413b2a5d78d233862b6595f9a8c4f48e944e60
Andrew Sung <theandrewasung@gmail.com>
2022-01-16 17:16:06 -0800

    Add invertColor option to ZPL label text(), line(), box(), ellipse().

--- src/zebra-zpl-label.ts
+++ src/zebra-zpl-label.ts
@@ -23,6 +23,7 @@ import {
     ZplGraphicDiagonalLine,
     ZplGraphicCircle,
     ZplGraphicEllipse,
+    ZplFieldReversePrint,
 } from './zebra-zpl-commands';
 
 type orientation = 'normal'|'top-down'|'upside-down'|'bottom-up';
@@ -103,35 +104,38 @@ export class ZplLabel
      * @param y - y-axis location (distance from top)
      * @param text - text segment
      * @param options.orientation - which way to orient the text
-     * @param options.font - name of a font or an object representing a font
+     * @param options.invertColor - chooses opposite color of background
+     * @param options.font        - name of a font or an object representing a font
      * 
      * @returns this ZPLLabel object, for chaining
      */
-    text(x : number, y : number, text : string, options? : {
+    text(x : number, y : number, text : string, options : {
         orientation? : orientation;
+        invertColor? : boolean;
         font? : string | {
             name: string;
             width: number;
             height?: number;
         };
-    }) {
-        const { orientation, font } = options || {};
-        const orientationCode = OrientationFromHumanReadable[orientation] || 'N';
+    } = {}) {
+        const orientation = OrientationFromHumanReadable[options.orientation] || 'N';
+        const invertColor = options.invertColor;
+        const font        = options.font;
 
         const zpl = [];
         zpl.push(ZplFieldOrigin.apply(this.toDots(x), this.toDots(y), 0)); // align from left for ease
-
+        if (invertColor) zpl.push(ZplFieldReversePrint.apply());
         if (font) {
             if (typeof font === 'string') {
-                zpl.push(ZplScalableFont.apply(font, orientationCode));
+                zpl.push(ZplScalableFont.apply(font, orientation));
             }
             else {
-                zpl.push(ZplScalableFont.apply(font.name, orientationCode, this.toDots(font.width), this.toDots(font.width || font.height)));
+                zpl.push(ZplScalableFont.apply(font.name, orientation, this.toDots(font.width), this.toDots(font.width || font.height)));
             }
         }
-        else if (this.lastOrientation !== orientationCode) {
-            zpl.push(ZplFieldOrientation.apply(orientationCode));
-            this.lastOrientation = orientationCode;
+        else if (this.lastOrientation !== orientation) {
+            zpl.push(ZplFieldOrientation.apply(orientation));
+            this.lastOrientation = orientation;
         }
 
         zpl.push(ZplFieldData.apply(text));
@@ -149,20 +153,24 @@ export class ZplLabel
      * @param y1 - starting y-axis location (distance from top)
      * @param x2 - starting x-axis location (distance from left)
      * @param y2 - starting y-axis location (distance from top)
-     * @param options.color     - color of the line
-     * @param options.thickness - thickness of the line
+     * @param options.color       - color of the line
+     * @param options.invertColor - chooses opposite color of background
+     * @param options.thickness   - thickness of the line
      * 
      * @returns this ZPLLabel object, for chaining
      */
     line(x1 : number, y1 : number, x2 : number, y2 : number, options : {
-        color?     : string;
-        thickness? : number;
+        color?       : string;
+        invertColor? : boolean;
+        thickness?   : number;
     } = {}) {
-        const thickness = options.thickness || 1;
-        const color     = ColorFromHumanReadable[options.color] || 'B';
+        const color       = ColorFromHumanReadable[options.color] || 'B';
+        const invertColor = options.invertColor;
+        const thickness   = options.thickness || 1;
 
         const zpl = [];
         zpl.push(ZplFieldOrigin.apply(this.toDots(Math.min(x1, x2)), this.toDots(Math.min(y1, y2)), 0)); // align from left for ease
+        if (invertColor) zpl.push(ZplFieldReversePrint.apply());
 
         if (x1 === x2 || y1 === y2) { // vertical or horizontal lines
             zpl.push(ZplGraphicBox.apply(this.toDots(x2 - x1), this.toDots(y2 - y1), thickness, color, 0));
@@ -175,7 +183,6 @@ export class ZplLabel
 
             zpl.push(ZplGraphicDiagonalLine.apply(this.toDots(Math.abs(width)), this.toDots(Math.abs(height)), thickness, color, orientation));
         }
-
         zpl.push(ZplFieldSeparator.apply());
 
         this.commands.push(zpl.join(''));
@@ -189,25 +196,29 @@ export class ZplLabel
      * @param y - y-axis location (distance from top)
      * @param width - width of the box
      * @param height - height of the box
-     * @param options.filled - fills the box with a solid color
-     * @param options.color - box or border color
+     * @param options.filled          - fills the box with a solid color
+     * @param options.color           - box or border color
+     * @param options.invertColor     - chooses opposite color of background
      * @param options.borderThickness - thickness of the border
-     * @param options.borderRadius - rounds the corners
+     * @param options.borderRadius    - rounds the corners
      * 
      * @returns this ZPLLabel object, for chaining
      */
     box(x : number, y : number, width : number, height : number, options : {
         filled?          : boolean;
         color?           : string;
+        invertColor?     : boolean;
         borderThickness? : number;
         borderRadius?    : number;
     } = {}) {
+        const invertColor     = options.invertColor;
         const borderThickness = options.filled ? this.toDots(Math.min(width, height)) : (options.borderThickness || 1);
         const borderRadius    = options.borderRadius || 0;
         const borderColor     = ColorFromHumanReadable[options.color] || 'B';
 
         const zpl = [];
         zpl.push(ZplFieldOrigin.apply(this.toDots(x), this.toDots(y), 0)); // align from left for ease
+        if (invertColor) zpl.push(ZplFieldReversePrint.apply());
         zpl.push(ZplGraphicBox.apply(this.toDots(width), this.toDots(height), borderThickness, borderColor, borderRadius));
         zpl.push(ZplFieldSeparator.apply());
 
@@ -222,9 +233,10 @@ export class ZplLabel
      * @param y - y-axis location (center or distance from top, depending on options.positioning)
      * @param width - width of the ellipse
      * @param height - height of the ellipse
-     * @param options.filled - fills the ellipse with a solid color
-     * @param options.color - ellipse or border color
-     * @param options.positioning - how to position the ellipse
+     * @param options.filled          - fills the ellipse with a solid color
+     * @param options.color           - ellipse or border color
+     * @param options.invertColor     - chooses opposite color of background
+     * @param options.positioning     - how to position the ellipse
      * @param options.borderThickness - thickness of the border
      * 
      * @returns this ZPLLabel object, for chaining
@@ -232,9 +244,11 @@ export class ZplLabel
     ellipse(x : number, y : number, width : number, height : number, options : {
         filled?          : boolean;
         color?           : string;
+        invertColor?     : boolean;
         positioning?     : 'center'|'top-left',
         borderThickness? : number,
     } = {}) {
+        const invertColor     = options.invertColor;
         const borderThickness = options.filled ? this.toDots(Math.min(width, height)) : (options.borderThickness || 1);
         const borderColor     = ColorFromHumanReadable[options.color] || 'B';
 
@@ -244,6 +258,7 @@ export class ZplLabel
 
         const zpl = [];
         zpl.push(ZplFieldOrigin.apply(this.toDots(left), this.toDots(top), 0)); // align from left for ease
+        if (invertColor) zpl.push(ZplFieldReversePrint.apply());
         if (width === height) {
             zpl.push(ZplGraphicCircle.apply(this.toDots(width), borderThickness, borderColor));
         }
