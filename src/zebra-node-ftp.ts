56b89bbea95a081b5f47f7c23543713ec53fc5ff
Andrew Sung <theandrewasung@gmail.com>
2022-12-19 11:56:43 -0800

    Update typing on zebra-node-ftp

--- src/zebra-node-ftp.ts
+++ src/zebra-node-ftp.ts
@@ -1,5 +1,5 @@
-import type { Server, AddressInfo } from 'net';
-import type { NetworkInterfaceInfo } from 'os';
+import type { AddressInfo } from 'net';
+import { NetworkInterfaceInfo, userInfo } from 'os';
 
 import { Socket, createServer } from 'net';
 import { networkInterfaces } from 'os';
@@ -9,6 +9,7 @@ import { basename } from 'path';
 const FTP_CONNECTION_SYMBOL = Symbol('CONNECT');
 const FTP_100_RETRY_SYMBOL  = Symbol('100-RETRY');
 
+type FTPResponse = { status : number, message : string };
 export class FTPResponseError extends Error {
     status : number;
 
@@ -19,19 +20,18 @@ export class FTPResponseError extends Error {
 }
 
 export class ZebraFTPClient {
-    private _socket : Socket;
-    private _sendQueue : Array<{
+    private _socket : null | Socket;
+    private _commandCallbackQueue : Array<{
         command  : string | symbol;
-        callback : (error : Error | void, output? : { status : number, message : string } ) => any;
+        callback : (error : Error | null, output? : FTPResponse ) => void;
     }>;
 
     private _logger  : (type : 'CMD' | 'RES' | 'ERR' | 'DEBUG', message : string) => void;
     private _connectionTimeoutMs : number;
     private _keepAliveIntervalMs : number;
 
-    private _connected : boolean;
-    private _connectionTimeout : NodeJS.Timeout;
-    private _keepAliveInterval : NodeJS.Timer;
+    private _connectionTimeout : null | NodeJS.Timeout;
+    private _keepAliveInterval : null | NodeJS.Timer;
 
     private host : string;
     private port : number;
@@ -51,28 +51,12 @@ export class ZebraFTPClient {
         connectionTimeout? : number;
     } = {}) {
         this._reset();
-
-        // Configuration Options
-        this._logger = (type, message) => options.logger ? options.logger(`[FTP] ${type} | ${message}`) : null;
-        this._keepAliveIntervalMs = options.keepAlive         || (60 * 1000);
-        this._connectionTimeoutMs = options.connectionTimeout || (5  * 1000);
-    }
-
-    /**
-     * Resets object to pre-connection status.
-     */
-    private _reset() : void {
-        if (this._socket)  this._socket.destroy();
-        if (this._connectionTimeout) clearTimeout(this._connectionTimeout);
-        if (this._keepAliveInterval) clearInterval(this._keepAliveInterval);
-
-        this._socket = null;
         
-        this._sendQueue     = [];
-
-        this._connected = false;
-        this._connectionTimeout = null;
-        this._keepAliveInterval = null;
+        // Configuration Options
+        const { logger, keepAlive, connectionTimeout } = options;
+        this._logger = (type, message) => logger?.(`[FTP] ${type} | ${message}`);
+        this._keepAliveIntervalMs = keepAlive         || (60 * 1000);
+        this._connectionTimeoutMs = connectionTimeout || (5  * 1000);
     }
 
     /**
@@ -86,46 +70,50 @@ export class ZebraFTPClient {
      * @returns out.message - FTP status message
      * @returns out.serverPort - port this FTP client is listening for active FTP connections
      */
-    async connect(host : string, port? : number, username? : string) : Promise<{ status : number, message : string, serverPort : number }> {
-        if (this._connected) throw new Error('FTP already connected');
+    async connect(host : string = '127.0.0.1', port : number = 21, username : string = userInfo().username) : Promise<FTPResponse> {
+        if (this._socket) throw new Error('FTP already connected');
 
         // (1) Initiate FTP socket connection 
-        this._socket = new Socket();
-        this._socket.setTimeout(0);
-        this._socket.setKeepAlive(true);
+        const socket = this._socket = new Socket();
+        socket.setTimeout(0);
+        socket.setKeepAlive(true);
 
         // (1a) Initialize Socket handlers
-        this._socket.once('connect', () => {
-            this._connected = true;
-
+        // Socket lifecycle handlers
+        socket.once('connect', () => {
             if (this._connectionTimeout) clearTimeout(this._connectionTimeout);
         });
-        this._socket.on('error', (error : Error) => {
-            this._logger('ERR', error.message);
+        socket.once('end',   () => this._reset());
+        socket.once('close', () => this._reset());
 
-            const { command, callback } = this._sendQueue.shift() || {};
-            if (!command || !callback) return;
-            
-            return callback(error);
-        });
-        this._socket.on('data', (chunk : Buffer) => {
+        // FTP connection response handlers
+        socket.on('data', (chunk : Buffer) => {
             const status  = parseInt(chunk.slice(0,3).toString());
             const message = chunk.slice(4, chunk.byteLength - 2).toString();
         
             this._logger('RES', `> ${status} ${message}`);
+                
+            const lastCommand = this._commandCallbackQueue.shift();
+            if (!lastCommand) return;
 
-            const { command, callback } = this._sendQueue.shift() || {};
-            if (!command || !callback) return;
-
+            const { callback } = lastCommand;
             if (status >= 400 && status < 600) {
                 return callback(new FTPResponseError(status, message));
             }
             return callback(null, { status, message });
         });
-        this._socket.once('end',   () => this._reset());
-        this._socket.once('close', () => this._reset());
+        socket.on('error', (error : Error) => {
+            this._logger('ERR', error.message);
 
-        const { status, message } = await new Promise((resolve, reject) => {
+            const lastCommand = this._commandCallbackQueue.shift();
+            if (!lastCommand) return;
+
+            const { callback } = lastCommand;
+            return callback(error);
+        });
+
+        // (1b) Connect socket to FTP port on printer
+        const connectionResponse = await new Promise<FTPResponse>((resolve, reject) => {
             let done = false;
 
             // Set connection timeout for printer
@@ -137,57 +125,54 @@ export class ZebraFTPClient {
                 return reject(new Error(`Exceeded ${this._connectionTimeoutMs}ms timeout while attempting to connect to printer.`));
             }, this._connectionTimeoutMs);
 
-            // Queue connection greeting response
-            this._sendQueue.push({
-                command : FTP_CONNECTION_SYMBOL,
-                callback(error, status) {
+            // Attempt to connect to printer (expects an initial connection greeting)
+            this._commandCallbackQueue.push({
+                command  : FTP_CONNECTION_SYMBOL,
+                callback : (error, res) => {
                     if (done) return;
                     done = true;
 
-                    if (error) {
+                    if (error || !res) {
                         this._reset();
                         return reject(error);
                     }
 
-                    return resolve(status);
+                    this.host = host;
+                    this.port = port;
+
+                    return resolve(res);
                 },
             });
 
-            // Execute commands
-            this.host = host || '127.0.0.1';
-            this.port = port || 21;
-            this._socket.connect(this.port, this.host);
+            socket.connect(port, host);
         });
 
         // (2) User Command
-        if (username) await this._send(`USER ${username}`);
+        await this._send(`USER ${username}`);
 
         // (3) Setup keep-alive NOOP interval
         if (this._keepAliveIntervalMs) {
             this._keepAliveInterval = setInterval(async () => {
-                if (!this._connected || this._sendQueue.length) return;
+                if (this._commandCallbackQueue.length) return;
 
-                try
-                {
+                try {
                     await this._send('NOOP');
                 }
-                catch (err)
-                {
+                catch (err) {
                     this._logger('ERR', err.message);
                     this._reset();
                 }
             }, this._keepAliveIntervalMs);
         }
 
-        const serverPort = 0;
-        return { status, message, serverPort };
+        return connectionResponse;
     }
 
     /**
      * Ends an FTP session.
      */
     async disconnect() : Promise<void> {
-        if (this._connected) await this._send('QUIT');
+        if (this._socket) await this._send('QUIT');
         this._reset();
     }
 
@@ -208,11 +193,10 @@ export class ZebraFTPClient {
      * @param data - data to be transferred to the printer (can come as a string, a buffer, a stream)
      * @param filename - name to transfer to the printer
      */
-    async putData(data : string | Uint8Array | NodeJS.ReadableStream, filename? : string) : Promise<void> {
-
+    async putData(data : string | Buffer | NodeJS.ReadableStream, filename : string = `${Date.now()}.zpl`) : Promise<void> {
         await this._send('TYPE I');
-        await this._sendActiveCommand(`STOR ${filename || (Date.now() + '.zpl')}`, (socket : Socket) => {
-            if (typeof data === 'string' || data instanceof Uint8Array) {
+        await this._sendActiveCommand(`STOR ${filename}`, (socket : Socket) => {
+            if (typeof data === 'string' || data instanceof Buffer) {
                 socket.end(data);
             }
             else {
@@ -222,39 +206,54 @@ export class ZebraFTPClient {
     }
 
     /**
+     * Resets object to pre-connection status.
+     */
+     private _reset() : void {
+        if (this._socket)  this._socket.destroy();
+        if (this._connectionTimeout) clearTimeout(this._connectionTimeout);
+        if (this._keepAliveInterval) clearInterval(this._keepAliveInterval);
+
+        this._socket = null;
+        this._connectionTimeout = null;
+        this._keepAliveInterval = null;
+
+        this._commandCallbackQueue = [];
+    }
+
+    /**
      * Sends an FTP command to the Zebra printer.
      * 
      * @param command - FTP command to send to the printer
      * @param onIntermediateResponse - callback for when 1xx responses are received. 1xx responses do not resolve the promise returned from this function, but will call this function if it is available
      */
-    private async _send(command? : string | typeof FTP_100_RETRY_SYMBOL, onIntermediateResponse? : (status : number, message : string) => any ) : Promise<{ status : number, message :string }> {
-        if (!this._connected) throw new Error('FTP not connected');
+    private async _send(command : string | typeof FTP_100_RETRY_SYMBOL, onIntermediateResponse? : (response: FTPResponse) => void ) : Promise<FTPResponse> {
+        return new Promise<FTPResponse>((resolve, reject) => {
+            if (!this._socket) throw new Error('Socket is not connected');
 
-        return new Promise((resolve, reject) => {
+            // Setup response callback
             let done = false;
-
-            // Await the response
-            this._sendQueue.push({
+            this._commandCallbackQueue.push({
                 command,
                 callback : (error, output) => {
                     if (done) return;
-                    done =true;
+                    done = true;
 
-                    if (error) return reject(error);
+                    if (error || !output) return reject(error);
                     
-                    // Expect another reply (1xx)
+                    // If status is in the 100 range, we should expect another reply (1xx)
                     if (output.status >= 100 && output.status < 200) {
-                        if (onIntermediateResponse) onIntermediateResponse(output.status, output.message);
+                        if (onIntermediateResponse) onIntermediateResponse(output);
 
-                        return resolve(this._send(FTP_100_RETRY_SYMBOL));
+                        return resolve(this._send(FTP_100_RETRY_SYMBOL, onIntermediateResponse));
                     }
 
-                    // Final response
+                    // Other statuses are final response
                     return resolve(output);
                 },
             })
 
-            // Send the command through the socket. If command is the FTP_100_RETRY_SYMBOL, just waiting for another response so we don't need to send them it again.
+            // Send the command through the socket.
+            // If command is the FTP_100_RETRY_SYMBOL, we just waiting for next reply instead of sending a command
             if (command !== FTP_100_RETRY_SYMBOL) {
                 const activeCommand = command.replace(/\s*$/, '');
                 this._logger('CMD', activeCommand);
@@ -269,58 +268,61 @@ export class ZebraFTPClient {
      * @param command - FTP command to send to the printer
      * @param onServerConnection - passed the socket when the printer reaches back out on the active command
      */
-    private async _sendActiveCommand(command : string, onServerConnection : (socket : Socket) => Promise<void> | void)
-    {
+    private async _sendActiveCommand(command : string, onServerConnection : (socket : Socket) => Promise<void> | void) {
+        if (!this._socket) throw new Error('Socket is not connected');
+
         // (A) Create a server to listen for active FTP responses
-        let server : Server;
-        const serverClosedPromise = new Promise(async (resolve) => {
-            server = createServer(async (socket : any) => {
+        let serverListener : (socket: Socket) => void = () => null;
+        const serverClosedPromise = new Promise<null>(async (resolve) => {
+            serverListener = async (socket) => {
                 socket.on('close', () => {
                     server.close(() => resolve(null))
                 });
 
                 // (D) Respond to server connection
                 await onServerConnection(socket);
-            });
+            }
         });
-        await new Promise(resolve => server.listen(0, () => resolve(null)));
+        const server = createServer(serverListener);
+        await new Promise(resolve => server.listen(0, () => resolve(null))); // listening on 0 tells system to assign a port
         const serverPort = (server.address() as AddressInfo).port;
 
-        // (B) Send PORT command to establish active connection
+        // (B) Send PORT command to printer socket to establish active connection
         // Fetch IPv4 address of this machine that is connected to the same network as the printer
-        function applyIPv4Netmask(address : string, netmask : string) {
-            const ipv4   : number[] = address.split('.').map((num : string) => parseInt(num));
-            const subnet : number[] = netmask.split('.').map((num : string) => parseInt(num));
-            
-            return ipv4.map((x : number, i : number) => (x & subnet[i]) ).join('.');
-        }
-
         const socketAddress  = this._socket.address() as AddressInfo;
-        const socketIp       = socketAddress.address ? socketAddress.address : null;
-        const sharedNetworkInterface = Object.values<NetworkInterfaceInfo[]>(networkInterfaces())
-            .map((networkInterface : NetworkInterfaceInfo[]) => {
-                const { address, netmask } = networkInterface.find(address => address.family === 'IPv4') || {};
+        const socketIp       = socketAddress?.address;
+        const sharedNetworkInterfaceIp = Object.values(networkInterfaces())
+            .map((networkInterface : NetworkInterfaceInfo[] | undefined) => {
+                const address = networkInterface?.find(address => address.family === 'IPv4');
                 if (!address) return null;
 
-                const interfaceSubnet = applyIPv4Netmask(address, netmask);
+                const { address: interfaceIp, netmask } = address;
+                const interfaceSubnet = applyIPv4Netmask(interfaceIp, netmask);
                 const socketSubnet    = applyIPv4Netmask(socketIp, netmask);
+                if (interfaceSubnet !== socketSubnet) return null;
 
-                return {
-                    address,
-                    subnet     : interfaceSubnet,
-                    sameSubnet : interfaceSubnet === socketSubnet,
-                };
+                return interfaceIp;
             })
-            .find(row => row && row.sameSubnet);
-        if (!sharedNetworkInterface) throw new Error('Unable to fetch corresponding network interface for socket.');
-
+            .find(ip => ip);
+        if (!sharedNetworkInterfaceIp) throw new Error('Unable to fetch corresponding network interface for socket.');
+        
+        // Send PORT command
         const portSixthDigit = serverPort % 256;
         const portFifthDigit = Math.trunc(serverPort / 256);
-        await this._send(`PORT ${[...sharedNetworkInterface.address.split('.'), portFifthDigit, portSixthDigit].join(',')}`);
+        await this._send(`PORT ${[...sharedNetworkInterfaceIp.split('.'), portFifthDigit, portSixthDigit].join(',')}`);
 
-        // (C) Send command
+        // (C) Send active command
         const commandCompletedPromise = this._send(command);
 
-        return await Promise.all([ serverClosedPromise, commandCompletedPromise ]);
+        await Promise.all([ serverClosedPromise, commandCompletedPromise ]);
+        return;
     }
+}
+
+// IPV4 Utils
+function applyIPv4Netmask(address : string, netmask : string) {
+    const ipv4   : number[] = address.split('.').map((num : string) => parseInt(num));
+    const subnet : number[] = netmask.split('.').map((num : string) => parseInt(num));
+
+    return ipv4.map((x : number, i : number) => (x & subnet[i]) ).join('.');
 }
